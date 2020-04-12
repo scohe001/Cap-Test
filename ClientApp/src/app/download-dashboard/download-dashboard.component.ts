20f7da83146e159feec3bc6b2114f975f395113e
scohe001 <scohe001@ucr.edu>
2020-04-11 22:42:56 -0500

    Got manual field validation working in test

--- ClientApp/src/app/download-dashboard/download-dashboard.component.ts
+++ ClientApp/src/app/download-dashboard/download-dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { TransactionType, TranType_TypeDef } from '../interfaces/transactiontype';
 import { TransactionmanagerService } from '../services/transactionmanager.service';
+import { CurrencyPipe } from '@angular/common';
 
 
 @Component({
@@ -12,7 +13,8 @@ import { TransactionmanagerService } from '../services/transactionmanager.servic
 export class DownloadDashboardComponent implements OnInit {
 
   constructor(private fb: FormBuilder,
-              private tranManager: TransactionmanagerService,) { }
+              private tranManager: TransactionmanagerService,
+              private currencyPipe: CurrencyPipe) { }
 
   // boring way:
   // tranForm: FormGroup = new FormGroup({
@@ -25,7 +27,7 @@ export class DownloadDashboardComponent implements OnInit {
   // cool way:
   tranForm: FormGroup = this.fb.group({
     tranType: ['', Validators.required],
-    tranAmount: [-1, Validators.min(0)],
+    tranAmount: ['$15.00', Validators.min(0.01)],
   })
 
   async ngOnInit() {
@@ -39,6 +41,7 @@ export class DownloadDashboardComponent implements OnInit {
   }
 
   onSubmit() {
+    this.validateAmountField(); // Make sure to validate before submission
     console.log("Submitting! With: ", this.tranForm.value);
     console.log(this.tranForm.value.tranType);
   }
@@ -55,6 +58,19 @@ export class DownloadDashboardComponent implements OnInit {
       tranAmount: 29.72,
     });
     this.tranForm.get('tranAmount').disable()
+  }
+
+  // When they tab out/click away from the amount, format it like a money val
+  validateAmountField() {
+    let currentAmountVal: string = this.tranForm.get('tranAmount').value;
+    if(! /^([0-9]+\.?[0-9]*)$/.test(currentAmountVal)) {
+      //Bad number format
+      currentAmountVal = '0';
+    }
+
+    this.tranForm.patchValue({
+      tranAmount: this.currencyPipe.transform(currentAmountVal)
+    })
   }
 
   // Formatted like this since it's being used as a callback

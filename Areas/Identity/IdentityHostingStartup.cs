fe5f91a3bab8a0c6aa798664fa4073763a25bb73
scohe001 <scohe001@ucr.edu>
2020-04-25 18:14:58 -0500

    Scaffolded login stuff and made it look passable

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+using System;
+using CreditCache.Data;
+using CreditCache.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CreditCache.Areas.Identity.IdentityHostingStartup))]
+namespace CreditCache.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
